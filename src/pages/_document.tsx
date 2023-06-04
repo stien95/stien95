24b377df5ec30f2b689f51589dd12e6b252079e7
stien95 <stivenvillegas258@gmail.com>
2023-06-04 15:41:52 -0500

    More icons to the page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,27 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Stien95 page, React Developer, dev of Tip Roulette (Project with Next + TS)" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <meta
+          name="description"
+          content="Stien95 page, React Developer, dev of Tip Roulette (Project with Next + TS)"
+        />
         <meta rel="canonical" content="https://stien95.github.io" />
         <link rel="manifest" href="/manifest.json" />
       </Head>

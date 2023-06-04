cc12ab949615e838da7302aa5ea003b8784d8e8f
stien95 <stivenvillegas258@gmail.com>
2023-06-04 15:36:25 -0500

    Manifest to html

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Stien95 page, React Developer, dev of Tip Roulette (Project with Next + TS)" />
         <meta rel="canonical" content="https://stien95.github.io" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <body>
         <div id="modal-root"></div>
